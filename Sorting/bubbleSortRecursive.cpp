d19809acadc74672f50a41520645cdd22d8e0651
Ragi Sai Ruthvik <121215909+RUTHVIKSR@users.noreply.github.com>
2025-08-03 11:07:18 +0530

    Create bubbleSortRecursive.cpp

---
+++ Sorting/bubbleSortRecursive.cpp
@@ -0,0 +1,16 @@
+void bubbleSort(vector<int> &nums, int n){
+    if(n == 1)
+        return;
+    int count = 0;
+    for(int i=0;i<n-1;i++){
+        if(nums[i] > nums[i+1]){
+            swap(nums[i], nums[i+1]);
+            count++;
+        }
+    }
+    
+    if(count == 0)
+        return;
+    bubbleSort(nums, n-1);
+    return;
+}
